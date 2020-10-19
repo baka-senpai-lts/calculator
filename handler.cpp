bae54b3ac02998ef8cedf41109d04862775e5a12
baka-senpai <baka.senpai.lts@gmail.com>
2020-10-19 20:21:13 +0300

    Add next_action()

--- handler.cpp
+++ handler.cpp
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <string>
 
 int get_length(std::string input)
@@ -88,7 +89,9 @@ int get_number(std::string input, int index)
             break;
 
         case '-':
-            num_index++;
+            if(i > 0) num_index++;
+            else if(input[i-1] != '(') num_index++;
+            else if (num_index == index) result += input[i];
             break;
 
         case '*':
@@ -131,9 +134,42 @@ int get_index(std::string input, char ch)
 }
 
 
+void set_index(std::string input, char ch, int& var)
+{
+    try
+    {
+        var = get_index(input, ch);
+    }
+    catch(...)
+    {
+        var = -1;
+    }
+}
+
+
+int next_action(std::string input)
+{
+    int mul, div, sum, sub;
+
+    set_index(input, '*', mul);
+    set_index(input, '/', div);
+    set_index(input, '+', sum);
+    set_index(input, '-', sub);
+
+    if(mul != -1 && div != -1) return mul > div ? mul : div;
+    if(mul != -1) return mul;
+    if(div != -1) return div;
+    if(sum != -1 && sub != -1) return sum > sub ? sum : sub;
+    if(sum != -1) return sum;
+    if(sub != -1) return sub;
+
+    return 0;
+}
+
+
 int calculate(std::string input)
 {
     if(get_length(input) == 1) return get_number(input, 0);
 
-    
+    return next_action(get_actions(input));
 }
