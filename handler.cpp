a6e24ea6c80b6a5622df0fd88b3b5b0d4e7d2d29
baka-senpai <baka.senpai.lts@gmail.com>
2020-10-19 12:56:30 +0300

    Add function get_number

--- handler.cpp
+++ handler.cpp
@@ -1,7 +1,44 @@
 #include <string>
 
 
+int get_number(std::string input, int index)
+{
+    std::string result;
+    int num_index = 0;
+
+    for(int i = 0; i < input.length(); i++)
+    {
+        switch(input[i])
+        {
+        case '+':
+            num_index++;
+            break;
+        case '-':
+            num_index++;
+            break;
+        case '*':
+            num_index++;
+            break;
+        case '/':
+            num_index++;
+            break;
+        default:
+            if(num_index == index)
+            {
+                result += input[i];
+            }
+            break;
+        }
+    }
+
+    if(result == "")
+        throw "Out of bounds";
+
+    return atoi(result.c_str());
+}
+
+
 int calculate(std::string input)
 {
-    return 12;
+    return get_number(input, 2);
 }
